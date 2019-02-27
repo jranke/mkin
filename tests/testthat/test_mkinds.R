3faf5bc087a4569e5b40f60b9349ad264654040b
Johannes Ranke <jranke@uni-bremen.de>
2019-02-27 13:40:05 +0100

    Fix test for mkinds printing

--- tests/testthat/test_mkinds.R
+++ tests/testthat/test_mkinds.R
@@ -20,5 +20,5 @@ context("Test dataset class mkinds used in gmkin")
 
 test_that("An mkinds object can be created and printed", {
   testdata <- mkinds$new("FOCUS C", data = FOCUS_2006_C, time_unit = "days", unit = "%AR")
-  expect_known_output(testdata, "FOCUS_2006_C_mkinds.txt")
+  expect_known_output(print(testdata), "FOCUS_2006_C_mkinds.txt")
 })
