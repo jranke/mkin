a7b8ec200358b6a331e76b3517c7ad21cc30346e
Johannes Ranke <jranke@uni-bremen.de>
2020-05-12 11:06:48 +0200

    Rerun tests and checks, CRAN comments

---
+++ cran-comments.md
@@ -0,0 +1,5 @@
+Update of yesterdays release, addressing a platform specific test outcome on Solaris that
+was revealed by the CRAN checks.
+
+This update also fixes a number of spelling errors that were found with the help of
+devtools::spell_check().
--- dumpdata.R
+++
@@ -1,7 +0,0 @@
-mkinpredict <-
-function (x, odeparms, odeini, outtimes = seq(0, 120, by = 0.10000000000000001), 
-    solution_type = "deSolve", use_compiled = "auto", method.ode = "lsoda", 
-    atol = 1e-08, rtol = 1e-10, map_output = TRUE, ...) 
-{
-    UseMethod("mkinpredict", x)
-}
