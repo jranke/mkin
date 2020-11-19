300c0fccf327b9959db39c256bae0974908e59b2
Johannes Ranke <jranke@uni-bremen.de>
2020-11-19 16:03:38 +0100

    Update tests, improve CAKE_export test

--- tests/testthat/test_CAKE_export.R
+++ tests/testthat/test_CAKE_export.R
@@ -6,10 +6,9 @@ test_that("Exporting is reproducible", {
               "FOCUS D" = FOCUS_2006_D),
     map = c(parent = "Parent", m1 = "M1"),
     links = c(parent = "m1"),
-    filename = "FOCUS_2006_D.csf", overwrite = TRUE, 
-    study = "FOCUS 2006 D")
+    filename = "FOCUS_2006_D.csf", overwrite = TRUE,
+    study = "FOCUS 2006 D", date = "Dummy date 0000-00-00")
   csf <- readLines(con = "FOCUS_2006_D.csf")
-  csf[8] <- "Date: Dummy date 0000-00-00"
   expect_known_value(csf, file = "FOCUS_2006_D.rds")
   expect_error(CAKE_export(ds = list("FOCUS C" = FOCUS_2006_C),
     filename = "FOCUS_2006_D.csf", overwrite = FALSE),
