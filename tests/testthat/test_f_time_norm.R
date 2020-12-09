b3488f035af01627e2c5ea8bee06b7af7202636e
Johannes Ranke <jranke@uni-bremen.de>
2020-12-09 14:43:40 +0100

    And more

--- tests/testthat/test_f_time_norm.R
+++ tests/testthat/test_f_time_norm.R
@@ -4,3 +4,8 @@ test_that("Simple temperature and moisture normalisation works", {
   expect_equal(round(f_time_norm_focus(25, 20, 25), 2), 1.37)
 })
 
+test_that("Time step normalisation for a dataset works", {
+  expect_output(f_time_norm_focus(D24_2014, study_moisture_ref_source = "focus", f_na = 1),
+    "was set to")
+  expect_equal(round(D24_2014$f_time_norm, 3), c(1.606, 0.712, 0.716, 0.716, 0.898))
+})
--- tests/testthat/test_mkinds.R
+++ tests/testthat/test_mkinds.R
@@ -1,6 +1,11 @@
-context("Test dataset class mkinds used in gmkin")
+context("Test dataset classes mkinds and mkindsg")
 
 test_that("An mkinds object can be created and printed", {
   testdata <- mkinds$new("FOCUS C", data = FOCUS_2006_C, time_unit = "days", unit = "%AR")
   expect_known_output(print(testdata), "FOCUS_2006_C_mkinds.txt")
+})
+
+test_that("An mkindsg object can be created and printed", {
+  testdata_group <- mkindsg$new("Experimental X", experimental_data_for_UBA_2019[6:10])
+  expect_known_output(print(testdata_group), "experimental_data_for_UBA_2019_mkindsg.txt")
 })
