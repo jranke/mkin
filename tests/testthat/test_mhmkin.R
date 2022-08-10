4572a31a440c967dfc66222c0214fccd087ac338
Johannes Ranke <jranke@uni-bremen.de>
2022-08-10 13:36:00 +0200

    Increase test coverage, require saemix version 3.1

--- tests/testthat/test_mhmkin.R
+++ tests/testthat/test_mhmkin.R
@@ -10,20 +10,21 @@ test_that("Multiple hierarchical kinetic models can be fitted and diagnosed", {
     update(fits_synth_const, error_model = "tc"))
 
   expect_known_output(
-    convergence(fits_synth_const),
-    "convergence_fits_synth_const.txt")
+    print(fits_synth_const),
+    "print_fits_synth_const.txt")
 
   hfits <- mhmkin(list(fits_synth_const, fits_synth_tc))
 
   expect_known_output(
-    convergence(hfits),
-    "convergence_hfits_synth.txt")
+    print(hfits),
+    "print_hfits_synth.txt")
 
   expect_known_output(
-    illparms(hfits),
+    print(illparms(hfits)),
     "illparms_hfits_synth.txt")
 
   expect_equal(which.min(AIC(hfits)), 3)
+  expect_equal(which.min(BIC(hfits)), 3)
 
   hfit_sfo_tc <- update(hfits[["SFO", "tc"]],
     covariance.model = diag(c(0, 1)))
