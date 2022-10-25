4030a3083d7ca51bc5d606dd95a877fb00967003
Johannes Ranke <jranke@uni-bremen.de>
2022-10-25 23:26:21 +0200

    Test multistart method

---
+++ tests/testthat/test_multistart.R
@@ -0,0 +1,15 @@
+test_that("multistart works for saem.mmix models", {
+  saem_biphasic_m_multi <- multistart(saem_biphasic_m, n = 8,
+    cores = n_cores)
+  expect_known_output(print(saem_biphasic_m_multi),
+    file = "print_multistart_biphasic.txt")
+
+  skip_on_cran()
+
+  llhist_biphasic <- function() llhist(saem_biphasic_m_multi)
+  parhist_biphasic <- function() parhist(saem_biphasic_m_multi,
+    ylim = c(0.5, 2))
+
+  vdiffr::expect_doppelganger("llhist for biphasic saemix fit", llhist_biphasic)
+  vdiffr::expect_doppelganger("parhist for biphasic saemix fit", parhist_biphasic)
+})
